c8438c0574dc700856ddbf4b196d9ec110de31e6
Bruno Habermann <bruno.habermann@dextra-sw.com>
2019-10-14 23:15:32 -0300

    First commit

---
+++ webapi/webapi/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace webapi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            args.ToList().ForEach(arg => Console.WriteLine(arg));
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
