e74bb4bb8ac7f036982fa3ab049b014e24611492
Bruno Habermann <bruno.habermann@dextra-sw.com>
2019-11-04 22:45:02 -0300

    Create a Repository interface

---
+++ webapi/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace webapi
+{
+    public interface IRepository<TEntity> where TEntity:class
+    {
+        IEnumerable<TEntity> Get();
+        TEntity GetById(object id);
+        void Insert(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(object id);
+    }
+}
